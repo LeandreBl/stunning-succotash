eb7b5b14d39cd6f84f913ac7b607beeb79f55217
leandre <leandre.blanchard@epitech.eu>
2020-11-09 11:30:45 +0100

    [fix] bug when multiple attendance for same student

--- stunning-succotash.py
+++ stunning-succotash.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!python3
 import pandas as pd
 import sys
 import re
@@ -6,7 +6,7 @@ from io import StringIO
 import difflib
 
 if len(sys.argv) != 3:
-    print(f"USAGE: {sys.argv[0]} <intra_list.csv> <attendance.csv>")
+    print(f'USAGE: {sys.argv[0]} <intra_list.csv> <attendance.csv>')
     exit(1)
 
 REPLACERS = {
@@ -25,23 +25,31 @@ def clean_string(string: str):
 
 
 def print_csv(dic: dict):
-    print("login;present")
+    print('login;present')
     for name, status in dic.items():
-        print(f"{name};{status}")
+        print(f'{name};{status}')
 
 
-with open(sys.argv[1], 'r', errors="ignore") as intra_csv:
+def remove_dot_spaces(entry: str):
+    return entry.replace(r'.|-', ' ')
+
+def initial_presence_dict(logins: list):
+    attendances = {}
+    for name in logins:
+        attendances[name] = 'absent'
+    return attendances
+
+with open(sys.argv[1], 'r', errors='ignore') as intra_csv:
     intra_csv = clean_string(intra_csv.read())
-    intra_csv = pd.read_csv(StringIO(intra_csv), sep=";")
-    with open(sys.argv[2], 'r', errors="ignore") as attendance_csv:
+    intra_csv = pd.read_csv(StringIO(intra_csv), sep=';')
+    with open(sys.argv[2], 'r', errors='ignore') as attendance_csv:
         string = clean_string(attendance_csv.read())
         attendance_csv = pd.read_csv(StringIO(string), index_col=None, header=0,
                                      skipinitialspace=True, sep='\t')
-        attendance = {}
-        for name in intra_csv["login"]:
-            attendance[name] = "absent"
-        for name in attendance_csv["nom complet"]:
-            names = difflib.get_close_matches(name, intra_csv["login"])
-            if len(names) == 1:
-                attendance[names[0]] = "present"
-        print_csv(attendance)
+        attendances = initial_presence_dict(intra_csv['login'])
+        for name in attendances.keys():
+            epur_name = remove_dot_spaces(name.replace('@epitech.eu', ''))
+            names = difflib.get_close_matches(epur_name, attendance_csv['nom complet'])
+            if len(names) > 0:
+                attendances[name] = 'present'
+        print_csv(attendances)
